1b43fb4f82f92ebd11a1117b1cea827d9c08e45e
timurkaff <timalololohka@gmail.com>
2024-05-26 12:56:03 +0300

    update

--- bot.js
+++ bot.js
@@ -1,14 +1,18 @@
 const { Telegraf } = require('telegraf');
 const { gpt } = require("gpti");
 
-const bot = new Telegraf('?');
-const chatMessages = new Map(); // Используем Map для хранения сообщений каждого пользователя
+const bot = new Telegraf('6584920107:AAEpqUBIq52cpbMHmqrTtUZ1gp06AWekrOI');
+const chatMessages = new Map();
 
-bot.start((ctx) => ctx.reply("Привет! Я GPT-4, одна из самых передовых искусственных интеллектуальных моделей, созданных в 2021 году. Я обучена на огромном объеме данных и знаю миллиарды параметров, позволяющих мне генерировать тексты, отвечать на вопросы и помогать людям во всем, что они могут понадобиться. Моя цель - помогать вам в решении задач и предоставлять информацию, доступную в Интернете. Чат сохраняеться и модель помнит твои запросы для очистки чаты напиши команду /clear"));
+bot.telegram.setMyCommands([
+    { command: '/clear', description: 'Очистить чат' }
+]);
+
+bot.start((ctx) => ctx.reply("Привет! Я GPT-4, одна из самых передовых искусственных интеллектуальных моделей, созданных в 2021 году. Я обучена на огромном объеме данных и знаю миллиарды параметров, позволяющих мне генерировать тексты, отвечать на вопросы и помогать людям во всем, что они могут понадобиться. Моя цель - помогать вам в решении задач и предоставлять информацию, доступную в Интернете. Чат сохраняется и модель помнит твои запросы. Для очистки чата напиши команду /clear"));
 
 bot.command('clear', (ctx) => {
     const userId = ctx.from.id;
-    chatMessages.delete(userId); // Удаляем сообщения для конкретного пользователя
+    chatMessages.delete(userId);
     ctx.reply('Чат был удален, модель не сохраняет предыдущие сообщения.');
 });
 
@@ -16,18 +20,15 @@ bot.on('text', async (ctx) => {
     const userId = ctx.from.id;
     const userText = ctx.message.text;
 
-    // Создаем массив сообщений для этого пользователя, если его еще нет
     if (!chatMessages.has(userId)) {
         chatMessages.set(userId, []);
     }
 
     const messagesForUser = chatMessages.get(userId);
     messagesForUser.push({ role: "user", content: userText });
+    await ctx.sendChatAction('typing');
 
-    // Отправляем сообщение "Печатаю..."
-    const typingMessage = await ctx.reply("Печатаю...");
-
-    gpt({
+    gpt.v1({
         messages: messagesForUser,
         prompt: userText,
         markdown: false,
@@ -37,15 +38,12 @@ bot.on('text', async (ctx) => {
             console.log(err);
             ctx.reply("Произошла ошибка при обработке запроса.");
         } else {
-            // Отправляем ответ от модели
             ctx.reply(data.gpt)
                 .then(() => {
-                    ctx.deleteMessage(typingMessage.message_id);
-                    messagesForUser.push({ role: "", content: userText });
-
+                    messagesForUser.push({ role: "assistant", content: data.gpt });
                 })
                 .catch((error) => {
-                    console.error("Ошибка при удалении сообщения:", error);
+                    console.error("Ошибка при отправке ответа:", error);
                 });
         }
     });
